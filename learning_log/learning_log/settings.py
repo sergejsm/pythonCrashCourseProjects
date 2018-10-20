a57c4dded85d90585a62df3de11ccfeb7378adbe
sergejsm <sergejs.mihailovs@btp-consultants.fr>
2018-10-20 17:21:53 +0200

    Fully functionning local version without styling

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+# My settings
+LOGIN_URL = '/users/login/'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
 
     # My apps
     'learning_logs',
+    'users'
 ]
 
 MIDDLEWARE = [
--- learning_log/learning_log/urls.py
+++ learning_log/learning_log/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'', include(('learning_logs.urls','learning_logs'), namespace = 'learning_logs')),
-
+    re_path(r'^users/', include(('users.urls','users'), namespace='users'))
 ]
